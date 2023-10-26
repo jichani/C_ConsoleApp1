cdfbe32760620ab4ab87f5f387e93344a3493fbd
jichani <jichan7571@naver.com>
2023-10-26 11:52:57 +0900

    3강 함수를 만드는 방법과 void, 오버로딩 설명

--- HelloCharpWin/Calculator.Designer.cs
+++ HelloCharpWin/Calculator.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             HelloLabel = new Label();
+            Sum1 = new TextBox();
+            Sum2 = new TextBox();
+            label1 = new Label();
+            SumNumbers = new Button();
+            SumResult = new TextBox();
             SuspendLayout();
             // 
             // HelloLabel
@@ -42,11 +47,57 @@
             HelloLabel.Text = "여기를 클릭하세요";
             HelloLabel.Click += HelloLabel_Click;
             // 
+            // Sum1
+            // 
+            Sum1.Location = new Point(27, 94);
+            Sum1.Name = "Sum1";
+            Sum1.Size = new Size(100, 23);
+            Sum1.TabIndex = 1;
+            // 
+            // Sum2
+            // 
+            Sum2.Location = new Point(186, 94);
+            Sum2.Name = "Sum2";
+            Sum2.Size = new Size(100, 23);
+            Sum2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(150, 98);
+            label1.Name = "label1";
+            label1.Size = new Size(15, 15);
+            label1.TabIndex = 3;
+            label1.Text = "+";
+            // 
+            // SumNumbers
+            // 
+            SumNumbers.Location = new Point(300, 95);
+            SumNumbers.Name = "SumNumbers";
+            SumNumbers.Size = new Size(75, 23);
+            SumNumbers.TabIndex = 4;
+            SumNumbers.Text = "=";
+            SumNumbers.UseVisualStyleBackColor = true;
+            SumNumbers.Click += SumNumbers_Click;
+            // 
+            // SumResult
+            // 
+            SumResult.Location = new Point(387, 95);
+            SumResult.Name = "SumResult";
+            SumResult.ReadOnly = true;
+            SumResult.Size = new Size(100, 23);
+            SumResult.TabIndex = 5;
+            // 
             // Calculator
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(560, 333);
+            Controls.Add(SumResult);
+            Controls.Add(SumNumbers);
+            Controls.Add(label1);
+            Controls.Add(Sum2);
+            Controls.Add(Sum1);
             Controls.Add(HelloLabel);
             Name = "Calculator";
             Text = "Form1";
@@ -57,5 +108,10 @@
         #endregion
 
         private Label HelloLabel;
+        private TextBox Sum1;
+        private TextBox Sum2;
+        private Label label1;
+        private Button SumNumbers;
+        private TextBox SumResult;
     }
 }
--- HelloCharpWin/Calculator.cs
+++ HelloCharpWin/Calculator.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace HelloCharpWin
 {
     public partial class Calculator : Form
@@ -17,6 +19,40 @@ namespace HelloCharpWin
             int sum = number1 + number2;
 
             HelloLabel.Text = sum.ToString();
+        }
+
+        private void SumNumbers_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(Sum1.Text);
+            int number2 = Convert.ToInt32(Sum2.Text);
+
+            int sum = Add(number1, number2);
+            SumResult.Text = sum.ToString();
+        }
+
+        // 정수형 더하기 함수를 생성하는 방법
+        public int Add(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            return sum;
+        }
+
+        // 실수형 더하기 함수를 생성하는 방법
+        public float Add(float number1, float number2)
+        {
+            float sum = number1 + number2;
+            return sum;
+        }
+
+        // 리턴 값이 없다는 것을 void로 쓴다.
+        // 함수의 오버로딩. 같은 이름과 같은 매개변수는 불가능. 매개변수가 다르다면 함수 이름이 같은 것을 몇개든 만들 수 있다.
+        // 리턴형은 중요하지 않다.
+
+        // 빼기 함수
+        public int Sub(int number1, int number2)
+        {
+            int sub = number1 - number2;
+            return sub;
         }
     }
 }
