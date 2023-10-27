e03d7204ba43ec81551e93685448633a998c90f3
jichani <jichan7571@naver.com>
2023-10-27 15:16:53 +0900

    7강 – 클래스 선언, 생성자, 접근 제한자

---
+++ CuteCat/Cat.cs
@@ -0,0 +1,61 @@
+﻿namespace CuteCat
+{
+    // internal은 default값으로, CuteCat이라는 namespace 안에서만 사용이 가능하다.
+    public class Cat
+    {
+        public string Name;
+        public int Age;
+
+        private int Happiness = 50;
+
+        public Cat (string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void GetBored ()
+        {
+            Happiness -= 10;
+
+            if (Happiness < 0)
+            {
+                Happiness = 0;
+            }
+        }
+
+        public void Play ()
+        {
+            Happiness += 10;
+
+            if (Happiness > 100)
+                Happiness = 100;
+        }
+
+        public void Eat ()
+        {
+            Happiness += 20;
+
+            if (Happiness > 100)
+                Happiness = 100;
+        }
+
+        public string Express()
+        {
+            string message = "";
+
+            if (Happiness >= 80)
+                message = "I'm very happy.";
+            else if (Happiness >= 60)
+                message = "I'm happy.";
+            else if (Happiness >= 40)
+                message = "I'm so so.";
+            else if (Happiness >= 20)
+                message = "I'm groomy.";
+            else
+                message = "I'm sad.";
+
+            return this.Name + ": " + message;
+        }
+    }
+}
---
+++ CuteCat/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace CuteCat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            CatScreen = new Label();
+            Play = new Button();
+            Feed = new Button();
+            CatTimer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // CatScreen
+            // 
+            CatScreen.BorderStyle = BorderStyle.Fixed3D;
+            CatScreen.Location = new Point(12, 9);
+            CatScreen.Name = "CatScreen";
+            CatScreen.Size = new Size(462, 54);
+            CatScreen.TabIndex = 0;
+            CatScreen.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // Play
+            // 
+            Play.Location = new Point(12, 81);
+            Play.Name = "Play";
+            Play.Size = new Size(123, 72);
+            Play.TabIndex = 1;
+            Play.Text = "Play";
+            Play.UseVisualStyleBackColor = true;
+            Play.Click += Play_Click;
+            // 
+            // Feed
+            // 
+            Feed.Location = new Point(154, 81);
+            Feed.Name = "Feed";
+            Feed.Size = new Size(123, 72);
+            Feed.TabIndex = 2;
+            Feed.Text = "Feed";
+            Feed.UseVisualStyleBackColor = true;
+            Feed.Click += Feed_Click;
+            // 
+            // CatTimer
+            // 
+            CatTimer.Enabled = true;
+            CatTimer.Interval = 5000;
+            CatTimer.Tick += CatTimer_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(487, 450);
+            Controls.Add(Feed);
+            Controls.Add(Play);
+            Controls.Add(CatScreen);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label CatScreen;
+        private Button Play;
+        private Button Feed;
+        private System.Windows.Forms.Timer CatTimer;
+    }
+}
